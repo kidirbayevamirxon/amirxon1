35824012751938bb513b91d1500307935923bc00
kidirbayevamirxon <amirxonkidirbayev0@gmail.com>
2024-11-05 17:06:07 +0500

    biti

--- global.js
+++ global.js
@@ -6,3 +6,12 @@ function toggleMenu() {
         menu.style.display = "none";
     }
 }
+
+function togglePhone() {
+    const phone = document.getElementById("phone");
+    if (phone.style.display === "none" || phone.style.display === "") {
+        phone.style.display = "block";
+    } else {
+        phone.style.display = "none";
+    }
+}
